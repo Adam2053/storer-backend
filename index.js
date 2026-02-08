43b8bb6448d77fbafad92d2529fdec4b952be536
Arpit Marathe <92802647+Adam2053@users.noreply.github.com>
2026-02-08 10:06:58 +0530

    Update CORS origin for frontend application

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "https://storer-frontend.vercel.app", // only allow your React frontend
+  origin: "https://arkive-saas.vercel.app", // only allow your React frontend
   credentials: true, // if you plan to send cookies
 }));
 
